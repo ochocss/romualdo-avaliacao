5b6ae642a9928d54fee0a96dcebfe5a1759dfff2
Fernando Hirsch <fernandobhirsch@gmail.com>
2024-10-18 01:06:02 -0300

    Add examples

--- src/br/edu/ifpr/lista/aplicacao/Main.java
+++ src/br/edu/ifpr/lista/aplicacao/Main.java
@@ -11,25 +11,39 @@ import br.edu.ifpr.lista.utils.CpfUtils;
 import br.edu.ifpr.lista.utils.Estado;
 
 public class Main {
-	public static void main(String[] args) {		
-		PFPersistencia persist = new PFPersistencia(List.of(
+	PFPersistencia persist;
+
+	Main() {
+		String cpf = CpfUtils.generateCpf(Estado.SP);
+		persist = new PFPersistencia(List.of(
 				new PessoaFisica("Gabriel Nicolas Caleb Pinto",
-                        LocalDate.of(1966,Month.MARCH,27),
-                        CpfUtils.generateCpf(Estado.PR), 
-                        "Rua Ednaldo", 190, "Barro", "Rolandia", "12345678")));
-		
-		System.out.println(persist.select());
-		
+						LocalDate.of(1966,Month.MARCH,27),
+						cpf,
+						"Rua Ednaldo", 190, "Barro", "Rolandia", "12345678")));
+		System.out.println("initialized: " + persist.select());
+
+
 		persist.insert(new PessoaFisica("Novo Povo",
-                        LocalDate.of(1966,Month.MARCH,27),
-                        CpfUtils.generateCpf(Estado.SP), 
-                        "Rua Ednaldo", 190, "Barro", "Rolandia", "12345678"));
-		
-		System.out.println(persist.select());
-		
-		
-		
-		
+				LocalDate.of(1966,Month.MARCH,27),
+				CpfUtils.generateCpf(Estado.PR),
+				"Rua Ednaldo", 190, "Barro", "Rolandia", "12345678"));
+		System.out.println("inserted: " + persist.select());
+
+		System.out.println("selected cpf " + cpf + ": " + persist.select(cpf));
+
+
+		persist.update(cpf, new PessoaFisica("Senhor atualizado",
+				LocalDate.of(1966,Month.MARCH,27),
+				CpfUtils.generateCpf(Estado.SP),
+				"Rua Ednaldo", 190, "Barro", "Rolandia", "12345678"));
+		System.out.println("updated: " + persist.select());
+
+		System.out.println("deleted " + persist.delete() + ": " + persist.select());
+	}
+
+	public static void main(String[] args) {
+		new Main();
+
 		List<PessoaJuridica> pjLista = List.of(
 				new PessoaJuridica("Casas Pernambucanas",
                     	  LocalDate.of(1908,Month.SEPTEMBER,25),
